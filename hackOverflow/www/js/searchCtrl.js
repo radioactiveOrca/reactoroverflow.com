952c4b74ada07721a0b5387714213a36e42503ae
jeremyhu9 <jeremyhu415@gmail.com>
2015-08-21 16:38:02 -0700

    Search on different page than post works

--- hackOverflow/www/js/searchCtrl.js
+++ hackOverflow/www/js/searchCtrl.js
@@ -2,12 +2,12 @@ angular.module('hackOverflow.search', [])
 
 .controller('SearchCtrl', function($scope, $state, Posts) {
   var word = $state.params.query;
-  $scope.getPosts = function() {
-    Posts.searchPosts(word, function(results){
-      console.log("word",results);
-      console.log('I am broadcasting the response data==== ', results);
-      $rootScope.$broadcast('showResults', results);
 
+
+  $scope.getPosts = function() {
+    $scope.data = {};
+    Posts.searchPosts(word, function(results){
+      $scope.data.posts = results;
     });
   };
 
