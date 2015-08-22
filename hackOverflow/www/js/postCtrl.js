786e140245370ed10c6ed13578a16e495ae41eff
Kiyomi Li <kshiraiw@gmail.com>
2015-08-21 19:07:43 -0700

    Remove console.log

--- hackOverflow/www/js/postCtrl.js
+++ hackOverflow/www/js/postCtrl.js
@@ -81,7 +81,6 @@ angular.module('hackOverflow.post', [])
 
   $scope.upVoteComment = function(comment, commentId) {
     //use commentID to send the user into the comment.upVotes array
-    console.log("clicked")
     if (comment.isUpvoted) {
       Comments.downVote(commentId).then(function(resp) {
         if (resp.status === 204) {
